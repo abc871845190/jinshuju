9571b799dce55a0b019eb95dcfd9c97f343f078a
huangsongzhang <871845190@qq.com>
2020-11-01 17:39:34 +0800

    金数据测试项目

--- src/main/java/com/example/jinshuju/controller/DataController.java
+++ src/main/java/com/example/jinshuju/controller/DataController.java
@@ -3,10 +3,13 @@ package com.example.jinshuju.controller;
 import com.example.jinshuju.pojo.Data;
 import com.example.jinshuju.service.DataService;
 import com.example.jinshuju.utils.ResultUtils.Result;
+import com.example.jinshuju.utils.ResultUtils.ResultUtils;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/DataController")
@@ -17,38 +20,53 @@ public class DataController {
     @Autowired(required = false)
     DataService dataService;
 
+    @ApiOperation(value = "添加表单数据", response = Result.class)
     @PostMapping("/Data")
     public Result insertData(@RequestBody Data data) {
         return dataService.insertData(data);
     }
 
+    @ApiOperation(value = "查看表单数据", response = Result.class)
     @GetMapping("/getAllData/{formId}")
     public Result getAllData(@PathVariable int formId) {
         return dataService.getAllData(formId);
     }
 
+    @ApiOperation(value = "查看填写所有人数", response = Result.class)
     @GetMapping("/getDataCount/{formId}")
     public Result getDataCount(@PathVariable int formId) {
         return dataService.getDataCount(formId);
     }
 
+    @ApiOperation(value = "查看当天填写人数", response = Result.class)
     @GetMapping("/getDataCountDaily/{formId}")
     public Result getDataCountDaily(@PathVariable int formId) {
         return dataService.getDataCountDaily(formId);
     }
 
+    @ApiOperation(value = "清空数据", response = Result.class)
     @DeleteMapping("/deleteAllData/{formId]")
     public Result deleteAllData(@PathVariable int formId) {
         return dataService.deleteAllData(formId);
     }
 
+    @ApiOperation(value = "删除单个数据项", response = Result.class)
     @DeleteMapping("/deleteData/{dataId}")
     public Result deleteData(@PathVariable int dataId) {
         return dataService.deleteData(dataId);
     }
 
-    @PutMapping("/update")
-    public Result update(@RequestBody Data data) {
+    @ApiOperation(value = "修改单个数据项", response = Result.class)
+    @PutMapping("/updateData")
+    public Result updateData(@RequestBody Data data) {
         return dataService.updateData(data);
+    }
+
+    @PostMapping("/upload")
+    public Result uploadExcel(@RequestParam("excelFile") MultipartFile excelFile) {
+        if (excelFile != null) {
+            return ResultUtils.success();
+        }
+        return ResultUtils.fail();
     }
 }
--- src/main/java/com/example/jinshuju/pojo/User.java
+++ src/main/java/com/example/jinshuju/pojo/User.java
@@ -8,16 +8,23 @@ import lombok.Data;
 @ApiModel(value = "User")
 @Data
 public class User {
+    @ApiModelProperty(value = "用户id", name = "userId", dataType = "int", example = "1")
     private int userId;
-    @ApiModelProperty(value = "用户名", name = "userName", dataType = "String", required = true, example = "test")
+    @ApiModelProperty(value = "用户名", name = "userName", dataType = "String", example = "test")
     private String userName;
+    @ApiModelProperty(value = "用户密码", name = "userPassword", dataType = "String", example = "123456")
     private String userPassword;
+    @ApiModelProperty(value = "用户电话", name = "userTelephone", dataType = "String", example = "123465")
     private String userTelephone;
+    @ApiModelProperty(value = "用户邮箱", name = "userEmail", dataType = "String", example = "123456@qq.com")
     private String userEmail;
+    @ApiModelProperty(value = "用户头像", name = "userImg", dataType = "String", example = "http://www.baodu.com")
     private String userImg;
     private int userEmailStatus;
     private int userEmailKey;
     private java.sql.Timestamp userEmailKeyTime;
+    @ApiModelProperty(value = "用户创建时间", name = "userCreateTime", dataType = "String", example = "2020-11-01 15:31:47")
     private java.sql.Timestamp userCreateTime;
+    @ApiModelProperty(value = "用户更新时间", name = "userUpdateTime", dataType = "String", example = "2020-11-01 15:31:47")
     private java.sql.Timestamp userUpdateTime;
 }
